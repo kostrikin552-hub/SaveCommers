de9a9870ec33ad0b52e953d5040accf8c6ad9ad8
kostrikin552-hub <kostrikin552@gmail.com>
2026-08-03 17:12:17 +0300

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const btn = document.getElementById('analyze-btn');
-    btn.addEventListener('click', function() {
-        alert('Кнопка работает!');
-    });
-});
