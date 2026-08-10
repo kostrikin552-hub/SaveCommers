8de8e6a600b8b0e5b6194cccb9543592bc3f6ea7
kostrikin552-hub <kostrikin552@gmail.com>
2026-08-10 12:37:15 +0300

    Update __init__.py

--- app/repositories/__init__.py
+++ app/repositories/__init__.py
@@ -1,1 +1,6 @@
-
+from .user_repo import *
+from .subscription_repo import *
+from .payment_repo import *
+from .referral_repo import *
+from .achievement_repo import *
+from .analysis_repo import *
